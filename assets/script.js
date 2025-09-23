71c86fce5c928a9f65db0a4bc2ce178b598f4654
Isabela <isaops22@gmail.com>
2025-09-22 23:48:32 -0300

    :salvo: initial commit

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+function toggleMode () {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    const img = document.querySelector("#profile img")
+
+
+
+}
+     
+     
+     
+     
+     
+     
+          
+ 
+
